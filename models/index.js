8a1797525b809c1f85d4fa54aaa1e0ea96562268
Revivedaniel <mlg.danielstark@gmail.com>
2021-09-19 11:51:06 -0700

    Created relationships between the models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  //Defining the third table that this connection is used in
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  //Defining the alias for the linked tables
+  as: "product_tag"
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  //Defining the third table that this connection is used in
+  through: {
+    model: ProductTag,
+    unique: FontFaceSetLoadEvent
+  },
+  //Defining the alias for the linked tables
+  as: "product"
+})
 
 module.exports = {
   Product,
