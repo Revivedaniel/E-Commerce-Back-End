672970d55781a14266ad396d1fc0983986ad5ee8
Revivedaniel <mlg.danielstark@gmail.com>
2021-09-19 11:00:30 -0700

    Created ProductTag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,34 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      //making the type of data stored in this column an integer
+      type: DataType.INTEGER,
+      //NOT NULL
+      allowNull: false,
+      //Primary Key
+      primaryKey: true,
+      //Auto increment
+      autoIncrement: true
+    },
+    product_id: {
+      //making the type of data stored in this column an integer
+      type: DataTypes.INTEGER,
+      //references the Product model's id
+      references: {
+        model: "product",
+        key: "id"
+      }
+    },
+    tag_id: {
+      //making the type of data stored in this column an integer
+      type: DataTypes.INTEGER,
+      //references the Tag model's id
+      references: {
+        model: "tag",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
